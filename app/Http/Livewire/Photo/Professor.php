14a0c9479129736ef010dc075f117345c36bc65f
AGoubs <arnaud@goubier.fr>
2021-09-09 16:39:16 +0200

    Create photo componement

---
+++ app/Http/Livewire/Photo/Professor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Photo;
+
+use Livewire\Component;
+
+class Professor extends Component
+{
+    public function render()
+    {
+        return view('livewire.photo.professor');
+    }
+}
