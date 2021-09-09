66c52f98dbeeb421f5b4a2901e0360bbb45594c7
AGoubs <arnaud@goubier.fr>
2021-09-09 14:50:22 +0200

    Prof display in dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Course;
 use App\Models\Lesson;
+use App\Models\Professor;
 use Livewire\Component;
 
 class Dashboard extends Component
@@ -11,11 +12,15 @@ class Dashboard extends Component
   public $courses;
   public $lessons;
   public $nbLessons;
+  public $professors;
+  public $nbProfessors;
   public $nbCourses;
 
   public function render()
   {
     $this->courses = Course::all();
+    $this->professors = Professor::all();
+    $this->nbProfessors = Professor::count();
     $this->lessons = Lesson::take(5)->orderBy('created_at', 'desc')->get();
     $this->nbLessons = Lesson::count();
     $this->nbCourses = count($this->courses);
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -275,7 +275,78 @@
            </div>
          </div>
        </div>
-       <div class="col-lg-7">
+       <div class="col-lg-7 col-md-6 mb-md-0 mb-4">
+        <div class="card">
+          <div class="card-header pb-0">
+            <div class="row">
+              <div class="col-lg-6 col-7">
+                <h6>Professeurs</h6>
+                <p class="text-sm mb-0">
+                  <i class="fa fa-check text-info" aria-hidden="true"></i>
+                  <span class="font-weight-bold ms-1">{{ $nbProfessors }} profs</span> cette année
+                </p>
+              </div>
+              <div class="col-lg-6 col-5 my-auto text-end">
+                <div class="dropdown float-lg-end pe-4">
+                  <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-ellipsis-v text-secondary"></i>
+                  </a>
+                  <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
+                    <li><a class="dropdown-item border-radius-md" href="javascript:;">Action</a>
+                    </li>
+                    <li><a class="dropdown-item border-radius-md" href="javascript:;">Another
+                        action</a></li>
+                    <li><a class="dropdown-item border-radius-md" href="javascript:;">Something
+                        else here</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-body px-0 pb-2">
+            <div class="table-responsive">
+              <table class="table align-items-center mb-0">
+                <thead>
+                  <tr>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                      Nom Prénom</th>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                      Photo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($professors as $professor)
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img src="../assets/img/small-logos/logo-jira.svg" class="avatar avatar-sm me-3">
+                          </div>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">
+                              <a
+                                href="{{ route('lesson.view', ['courseId' => $professor->id]) }}">{{ $professor->firstname }} {{ $professor->lastname }}</a>
+                            </h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <div class="avatar-group mt-2">
+                          <a href="javascript:;" class="avatar avatar-sm rounded-circle" data-bs-toggle="tooltip"
+                            data-bs-placement="bottom" title="Ryan Tompson">
+                            <img alt="Image placeholder" src="../assets/img/team-4.jpg">
+                          </a>
+                        </div>
+                      </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+       {{-- <div class="col-lg-7">
          <div class="card">
            <div class="card-header pb-0">
              <h6>Sales overview</h6>
@@ -290,7 +361,7 @@
              </div>
            </div>
          </div>
-       </div>
+       </div> --}}
      </div>
    </div>
  </main>
