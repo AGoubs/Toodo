e94ab84e525c0f7e7801ec3be1166d423d45e957
AGoubs <arnaud@goubier.fr>
2021-09-13 11:15:48 +0200

    Upgrade interface

--- resources/views/livewire/photo/big-professor.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-  @if (isset($photo) && file_exists(public_path('storage/photos/' . $photo)))
-    <img src="{{ asset('storage/photos/' . $photo) }}" alt="profile picture" class="w-100 border-radius-lg shadow-sm">
-
-  @else
-    <img src="../assets/img/user-placeholder.png" alt="..." class="w-100 border-radius-lg shadow-sm">
-  @endif
-</div>
--- resources/views/livewire/photo/professor.blade.php
+++ resources/views/livewire/photo/professor.blade.php
@@ -1,15 +1,19 @@
 <div>
-  @if (isset($size))
-    <a href="{{ route('professor.read', ['professorId' => $professor->id]) }}" class="avatar avatar-{{ $size }} rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ $professor->firstname }} {{ $professor->lastname }}">
+  @if (isset($professor->photo))
+
+    @if (isset($size))
+      <a href="{{ route('professor.read', ['professorId' => $professor->id]) }}" class="avatar avatar-{{ $size }} rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ $professor->firstname }} {{ $professor->lastname }}">
+    @endif
+
+    @if (file_exists(public_path('storage/photos/' . $professor->photo)))
+      <img src="{{ asset('storage/photos/' . $professor->photo) }}" alt="profile picture" class="w-100 border-radius-lg shadow-sm">
+    @else
+      <img src="../assets/img/user-placeholder.png" alt="..." class="w-100 border-radius-lg shadow-sm">
+    @endif
+
+    @if (isset($size))
+      </a>
+    @endif
   @endif
 
-  @if (isset($professor->photo) && file_exists(public_path('storage/photos/' . $professor->photo)))
-    <img src="{{ asset('storage/photos/' . $professor->photo) }}" alt="profile picture" class="w-100 border-radius-lg shadow-sm">
-  @else
-    <img src="../assets/img/user-placeholder.png" alt="..." class="w-100 border-radius-lg shadow-sm">
-  @endif
-
-  @if (isset($size))
-    </a>
-  @endif
 </div>
