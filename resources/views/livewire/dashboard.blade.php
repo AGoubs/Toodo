5c94bef7d9ec900fe6853940d3b8dd5110d7a38f
AGoubs <arnaud@goubier.fr>
2021-09-14 10:48:45 +0200

    Add calendar

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,7 +1,7 @@
 <main>
   <div class="container-fluid py-4">
     <div class="row ">
-      <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
+      <div class="col-lg-7 col-md-6 mb-md-0 mb-4">
         <div class="card">
           <div class="card-header pb-0">
             <div class="row">
@@ -36,37 +36,33 @@
                 </thead>
                 <tbody>
                   @foreach ($courses as $course)
-                  <tr>
-                    <td>
-                      <div class="d-flex px-2 py-1">
-                        <div>
-                          <img src="../assets/img/small-logos/logo-jira.svg" class="avatar avatar-sm me-3">
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img src="../assets/img/small-logos/logo-jira.svg" class="avatar avatar-sm me-3">
+                          </div>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">
+                              <a href="{{ route('lesson.view', ['courseId' => $course->id]) }}">{{ $course->name }}</a>
+                            </h6>
+                          </div>
                         </div>
-                        <div class="d-flex flex-column justify-content-center">
-                          <h6 class="mb-0 text-sm">
-                            <a href="{{ route('lesson.view', ['courseId' => $course->id]) }}">{{ $course->name }}</a>
-                          </h6>
+                      </td>
+                      <td>
+                        <div class="avatar-group">
+                          @livewire('photo.professor', ['professorId' => $course->professor_id, 'size' => 'xs'])
                         </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="avatar-group">
-                        @livewire('photo.professor', ['professorId' => $course->professor_id, 'size' => 'xs'])
-                      </div>
-                    </td>
-                    <td class="text-sm font-weight-bold text-center">
-                      <span class="my-2 text-sm"><a
-                          href="{{route('course.read', ['courseId' => $course->id])}}" class="mx-3"
-                          data-bs-toggle="tooltip" data-bs-original-title="Editer">
-                          <i class="fas fa-user-edit text-secondary" aria-hidden="true"></i>
-                        </a>
-                        <span>
-                          <i onclick="confirm('Voulez-vous supprimer ce cours ?') || event.stopImmediatePropagation()"
-                            wire:click="deleteCourse({{$course->id}})" class="cursor-pointer fas fa-trash text-secondary"
-                            data-bs-toggle="tooltip" data-bs-original-title="Supprimer" aria-hidden="true"></i>
-                        </span></span>
-                    </td>
-                  </tr>
+                      </td>
+                      <td class="text-sm font-weight-bold text-center">
+                        <span class="my-2 text-sm"><a href="{{ route('course.read', ['courseId' => $course->id]) }}" class="mx-3" data-bs-toggle="tooltip" data-bs-original-title="Editer">
+                            <i class="fas fa-user-edit text-secondary" aria-hidden="true"></i>
+                          </a>
+                          <span>
+                            <i onclick="confirm('Voulez-vous supprimer ce cours ?') || event.stopImmediatePropagation()" wire:click="deleteCourse({{ $course->id }})" class="cursor-pointer fas fa-trash text-secondary" data-bs-toggle="tooltip" data-bs-original-title="Supprimer" aria-hidden="true"></i>
+                          </span></span>
+                      </td>
+                    </tr>
                   @endforeach
                 </tbody>
               </table>
@@ -74,7 +70,7 @@
           </div>
         </div>
       </div>
-      <div class="col-lg-4 col-md-6">
+      <div class="col-lg-5 col-md-6">
         <div class="card h-100">
           <div class="card-header pb-0">
             <h6>Dernières leçons publiées</h6>
@@ -86,19 +82,19 @@
           <div class="card-body p-3">
             <div class="timeline timeline-one-side">
               @foreach ($lessons as $lesson)
-              <div class="timeline-block mb-3">
-                <span class="timeline-step">
-                  <i class="ni ni-book-bookmark text-success text-gradient"></i>
-                </span>
-                <div class="timeline-content">
-                  <h6 class="text-dark text-sm font-weight-bold mb-0">
-                    <a href="{{ route('lesson.read', ['lessonId' => $lesson->id, 'courseId' => $lesson->course_id]) }}">
-                      {{ $lesson->name }}</a>
-                  </h6>
-                  <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">
-                    {{ strtoupper(date('d M h:m', strtotime($lesson->created_at))) }}</p>
+                <div class="timeline-block mb-3">
+                  <span class="timeline-step">
+                    <i class="ni ni-book-bookmark text-success text-gradient"></i>
+                  </span>
+                  <div class="timeline-content">
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">
+                      <a href="{{ route('lesson.read', ['lessonId' => $lesson->id, 'courseId' => $lesson->course_id]) }}">
+                        {{ $lesson->name }}</a>
+                    </h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">
+                      {{ strtoupper(date('d M h:m', strtotime($lesson->created_at))) }}</p>
+                  </div>
                 </div>
-              </div>
               @endforeach
 
 
@@ -108,7 +104,7 @@
       </div>
     </div>
     <div class="row mt-4">
-      <div class="col-lg-7 col-md-12 mb-md-0 mb-4">
+      <div class="col-lg-4 col-md-12 mb-md-0 mb-4">
         <div class="card">
           <div class="card-header pb-0">
             <div class="row">
@@ -117,8 +113,7 @@
                   <div>
                     <h5 class="mb-0">Professeurs</h5>
                   </div>
-                  <a href="{{ route('professor.create') }}" class="btn bg-gradient-dark btn-sm mb-0"
-                    type="button">+&nbsp;
+                  <a href="{{ route('professor.create') }}" class="btn bg-gradient-dark btn-sm mb-0" type="button">+&nbsp;
                     Ajouter</a>
                 </div>
                 <p class="text-sm mb-0">
@@ -141,26 +136,26 @@
                 </thead>
                 <tbody>
                   @foreach ($professors as $professor)
-                  <tr>
-                    <td>
-                      <div class="d-flex px-2 py-1">
-                        <div>
-                          <img src="../assets/img/small-logos/logo-jira.svg" class="avatar avatar-sm me-3">
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img src="../assets/img/small-logos/logo-jira.svg" class="avatar avatar-sm me-3">
+                          </div>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">
+                              <a href="{{ route('professor.read', ['professorId' => $professor->id]) }}">{{ $professor->firstname }}
+                                {{ $professor->lastname }}</a>
+                            </h6>
+                          </div>
                         </div>
-                        <div class="d-flex flex-column justify-content-center">
-                          <h6 class="mb-0 text-sm">
-                            <a href="{{ route('professor.read', ['professorId' => $professor->id]) }}">{{ $professor->firstname }}
-                              {{ $professor->lastname }}</a>
-                          </h6>
+                      </td>
+                      <td>
+                        <div class="avatar-group">
+                          @livewire('photo.professor', ['professorId' => $professor->id, 'size' => 'sm'])
                         </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="avatar-group">
-                        @livewire('photo.professor', ['professorId' => $professor->id, 'size' => 'sm'])
-                      </div>
-                    </td>
-                  </tr>
+                      </td>
+                    </tr>
                   @endforeach
                 </tbody>
               </table>
@@ -168,165 +163,76 @@
           </div>
         </div>
       </div>
+      <div class="col-lg-8">
+        <div class="card widget-calendar">
+          <!-- Card header -->
+          <div class="card-header pb-0">
+            <div class="row">
+              <div class="col-lg-12 col-12">
+                <div class="d-flex flex-row justify-content-between">
+                  <div>
+                    <h5 class="mb-0">Calendrier</h5>
+                  </div>
+                  <a href="{{ route('professor.create') }}" class="btn bg-gradient-dark btn-sm mb-0" type="button">+&nbsp;
+                    Ajouter</a>
+                </div>
+                <div class="d-flex">
+                  <div class="p text-sm font-weight-bold mb-0 widget-calendar-day"></div>
+                  <span>,&nbsp;</span>
+                  <div class="p text-sm font-weight-bold mb-1 widget-calendar-year"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Card body -->
+          <div class="card-body p-3">
+            <div class="calendar" data-bs-toggle="calendar" id="calendar"></div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
 </main>
 
-<!--   Core JS Files   -->
-<script src="../assets/js/plugins/chartjs.min.js"></script>
-<script src="../assets/js/plugins/Chart.extension.js"></script>
+<script src="../../../assets/js/plugins/fullcalendar.min.js"></script>
+<script src="../../../assets/js/plugins/locales-all.min.js"></script>
+
 <script>
-  var ctx = document.getElementById("chart-bars").getContext("2d");
+  var calendar = new FullCalendar.Calendar(document.getElementById("calendar"), {
+    initialView: "dayGridMonth",
+    height: 550,
+    headerToolbar: {
+      start: 'title', // will normally be on the left. if RTL, will be on the right
+      center: '',
+      end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
+    },
+    selectable: true,
+    editable: true,
+    initialDate: '2020-12-01',
+    events: [],
+    views: {
+      month: {
+        titleFormat: {
+          month: "long",
+          year: "numeric"
+        }
+      },
+      agendaWeek: {
+        titleFormat: {
+          month: "long",
+          year: "numeric",
+          day: "numeric"
+        }
+      },
+      agendaDay: {
+        titleFormat: {
+          month: "short",
+          year: "numeric",
+          day: "numeric"
+        }
+      }
+    },
+  });
 
-   new Chart(ctx, {
-     type: "bar",
-     data: {
-       labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-       datasets: [{
-         label: "Sales",
-         tension: 0.4,
-         borderWidth: 0,
-         pointRadius: 0,
-         backgroundColor: "#fff",
-         data: [450, 200, 100, 220, 500, 100, 400, 230, 500],
-         maxBarThickness: 6
-       }, ],
-     },
-     options: {
-       responsive: true,
-       maintainAspectRatio: false,
-       legend: {
-         display: false,
-       },
-       tooltips: {
-         enabled: true,
-         mode: "index",
-         intersect: false,
-       },
-       scales: {
-         yAxes: [{
-           gridLines: {
-             display: false,
-           },
-           ticks: {
-             suggestedMin: 0,
-             suggestedMax: 500,
-             beginAtZero: true,
-             padding: 0,
-             fontSize: 14,
-             lineHeight: 3,
-             fontColor: "#fff",
-             fontStyle: 'normal',
-             fontFamily: "Open Sans",
-           },
-         }, ],
-         xAxes: [{
-           gridLines: {
-             display: false,
-           },
-           ticks: {
-             display: false,
-             padding: 20,
-           },
-         }, ],
-       },
-     },
-   });
-
-   var ctx2 = document.getElementById("chart-line").getContext("2d");
-
-   var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
-
-   gradientStroke1.addColorStop(1, 'rgba(253,235,173,0.4)');
-   gradientStroke1.addColorStop(0.2, 'rgba(245,57,57,0.0)');
-   gradientStroke1.addColorStop(0, 'rgba(255,214,61,0)'); //purple colors
-
-   var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
-
-   gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.4)');
-   gradientStroke2.addColorStop(0.2, 'rgba(245,57,57,0.0)');
-   gradientStroke2.addColorStop(0, 'rgba(255,214,61,0)'); //purple colors
-
-
-   new Chart(ctx2, {
-     type: "line",
-     data: {
-       labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-       datasets: [{
-           label: "Mobile apps",
-           tension: 0.4,
-           borderWidth: 0,
-           pointRadius: 0,
-           borderColor: "#fbcf33",
-           borderWidth: 3,
-           backgroundColor: gradientStroke1,
-           data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
-           maxBarThickness: 6
-
-         },
-         {
-           label: "Websites",
-           tension: 0.4,
-           borderWidth: 0,
-           pointRadius: 0,
-           borderColor: "#f53939",
-           borderWidth: 3,
-           backgroundColor: gradientStroke2,
-           data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
-           maxBarThickness: 6
-
-         },
-       ],
-     },
-     options: {
-       responsive: true,
-       maintainAspectRatio: false,
-       legend: {
-         display: false,
-       },
-       tooltips: {
-         enabled: true,
-         mode: "index",
-         intersect: false,
-       },
-       scales: {
-         yAxes: [{
-           gridLines: {
-             borderDash: [2],
-             borderDashOffset: [2],
-             color: '#dee2e6',
-             zeroLineColor: '#dee2e6',
-             zeroLineWidth: 1,
-             zeroLineBorderDash: [2],
-             drawBorder: false,
-           },
-           ticks: {
-             suggestedMin: 0,
-             suggestedMax: 500,
-             beginAtZero: true,
-             padding: 10,
-             fontSize: 11,
-             fontColor: '#adb5bd',
-             lineHeight: 3,
-             fontStyle: 'normal',
-             fontFamily: "Open Sans",
-           },
-         }, ],
-         xAxes: [{
-           gridLines: {
-             zeroLineColor: 'rgba(0,0,0,0)',
-             display: false,
-           },
-           ticks: {
-             padding: 10,
-             fontSize: 11,
-             fontColor: '#adb5bd',
-             lineHeight: 3,
-             fontStyle: 'normal',
-             fontFamily: "Open Sans",
-           },
-         }, ],
-       },
-     },
-   });
+  calendar.setOption('locale', 'fr');
+  calendar.render();
 </script>
