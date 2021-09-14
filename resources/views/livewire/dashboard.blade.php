95c0c6475c4e9e7af9d64fc52fcb60cf047e37af
AGoubs <arnaud@goubier.fr>
2021-09-14 13:04:58 +0200

    Calendar + dark timeline

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -71,9 +71,9 @@
         </div>
       </div>
       <div class="col-lg-5 col-md-6">
-        <div class="card h-100">
-          <div class="card-header pb-0">
-            <h6>Dernières leçons publiées</h6>
+        <div class="card bg-gradient-dark h-100">
+          <div class="card-header bg-transparent pb-0">
+            <h6 class="text-white">Dernières leçons publiées</h6>
             <p class="text-sm">
               <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
               <span class="font-weight-bold">{{ $nbLessons }}</span> leçons
@@ -83,12 +83,12 @@
             <div class="timeline timeline-one-side">
               @foreach ($lessons as $lesson)
                 <div class="timeline-block mb-3">
-                  <span class="timeline-step">
+                  <span class="timeline-step bg-dark">
                     <i class="ni ni-book-bookmark text-success text-gradient"></i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">
-                      <a href="{{ route('lesson.read', ['lessonId' => $lesson->id, 'courseId' => $lesson->course_id]) }}">
+                    <h6 class="text-white text-sm font-weight-bold mb-0">
+                      <a href="{{ route('lesson.read', ['lessonId' => $lesson->id, 'courseId' => $lesson->course_id]) }}" style="color: white">
                         {{ $lesson->name }}</a>
                     </h6>
                     <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">
@@ -164,75 +164,7 @@
         </div>
       </div>
       <div class="col-lg-8">
-        <div class="card widget-calendar">
-          <!-- Card header -->
-          <div class="card-header pb-0">
-            <div class="row">
-              <div class="col-lg-12 col-12">
-                <div class="d-flex flex-row justify-content-between">
-                  <div>
-                    <h5 class="mb-0">Calendrier</h5>
-                  </div>
-                  <a href="{{ route('professor.create') }}" class="btn bg-gradient-dark btn-sm mb-0" type="button">+&nbsp;
-                    Ajouter</a>
-                </div>
-                <div class="d-flex">
-                  <div class="p text-sm font-weight-bold mb-0 widget-calendar-day"></div>
-                  <span>,&nbsp;</span>
-                  <div class="p text-sm font-weight-bold mb-1 widget-calendar-year"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- Card body -->
-          <div class="card-body p-3">
-            <div class="calendar" data-bs-toggle="calendar" id="calendar"></div>
-          </div>
-        </div>
+        @livewire('components.calendar')
       </div>
     </div>
 </main>
-
-<script src="../../../assets/js/plugins/fullcalendar.min.js"></script>
-<script src="../../../assets/js/plugins/locales-all.min.js"></script>
-
-<script>
-  var calendar = new FullCalendar.Calendar(document.getElementById("calendar"), {
-    initialView: "dayGridMonth",
-    height: 550,
-    headerToolbar: {
-      start: 'title', // will normally be on the left. if RTL, will be on the right
-      center: '',
-      end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
-    },
-    selectable: true,
-    editable: true,
-    initialDate: '2020-12-01',
-    events: [],
-    views: {
-      month: {
-        titleFormat: {
-          month: "long",
-          year: "numeric"
-        }
-      },
-      agendaWeek: {
-        titleFormat: {
-          month: "long",
-          year: "numeric",
-          day: "numeric"
-        }
-      },
-      agendaDay: {
-        titleFormat: {
-          month: "short",
-          year: "numeric",
-          day: "numeric"
-        }
-      }
-    },
-  });
-
-  calendar.setOption('locale', 'fr');
-  calendar.render();
-</script>
