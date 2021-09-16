6e18389aa7fd6fc3f642c67e07d7bf7f885f37ed
AGoubs <arnaud@goubier.fr>
2021-09-16 10:13:35 +0200

    read picture modification

--- app/Http/Livewire/Professor/Read.php
+++ app/Http/Livewire/Professor/Read.php
@@ -4,17 +4,22 @@ namespace App\Http\Livewire\Professor;
 
 use App\Models\Professor;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Read extends Component
 {
+  use WithFileUploads;
+
   public $professor;
+  public $photo;
 
   public $professorId;
   protected $queryString = ['professorId'];
 
   protected $rules = [
     'professor.firstname' => 'required|string',
-    'professor.lastname' => 'required|string'
+    'professor.lastname' => 'required|string',
+    'photo' => ''
   ];
 
   public function render()
@@ -31,6 +36,13 @@ class Read extends Component
   public function submit()
   {
     $this->validate();
+
+    if ($this->photo) {
+      $name = md5($this->photo . microtime()) . '.' . $this->photo->extension();
+      $this->photo->storeAs('photos', $name);
+      $this->professor->photo = $name;
+    }
+
     $this->professor->save();
     session()->flash('success', 'Modification effectuée avec succès !');
   }
--- resources/views/livewire/professor/read.blade.php
+++ resources/views/livewire/professor/read.blade.php
@@ -3,16 +3,12 @@
     <div class="row mb-5 justify-content-center align-items-center">
       <div class="col-lg-9 col-md-12">
         @if (session()->has('success'))
-        <div
-          class="alert position-absolute top-0 border-0 text-white w-50 end-0 start-0 mt-2 mx-auto py-2 alert-success"
-          style=" opacity: 1 !important; transition: all 0.35s ease 0s;" x-data="{ show: true }" x-show="show"
-          x-init="setTimeout(() => show = false, 3000)">
-          <div class="d-flex mb-1">
-            <div class="alert-icon me-1"><i class="ni ni-bell-55 mt-1"></i></div><span
-              class="alert-text"><strong>Succès</strong></span>
+          <div class="alert position-absolute top-0 border-0 text-white w-50 end-0 start-0 mt-2 mx-auto py-2 alert-success" style=" opacity: 1 !important; transition: all 0.35s ease 0s;" x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)">
+            <div class="d-flex mb-1">
+              <div class="alert-icon me-1"><i class="ni ni-bell-55 mt-1"></i></div><span class="alert-text"><strong>Succès</strong></span>
+            </div>
+            <span class="text-sm">{{ session()->get('success') }}</span>
           </div>
-          <span class="text-sm">{{ session()->get('success') }}</span>
-        </div>
         @endif
 
         <!-- Card Profile -->
@@ -22,16 +18,18 @@
               <form wire:submit.prevent="submit">
                 <div class="avatar avatar-xl position-relative">
                   <div>
-                    <label for="file-input"
-                      class="btn btn-sm btn-icon-only bg-gradient-light position-absolute bottom-0 end-0 mb-n2 me-n2">
-                      <i class="fa fa-pen top-0" data-bs-toggle="tooltip" data-bs-placement="top" title=""
-                        aria-hidden="true" data-bs-original-title="Edit Image" aria-label="Edit Image"></i><span
-                        class="sr-only">Edit Image</span>
+                    <label for="file-input" class="btn btn-sm btn-icon-only bg-gradient-light position-absolute bottom-0 end-0 mb-n2 me-n2">
+                      <i class="fa fa-pen top-0" data-bs-toggle="tooltip" data-bs-placement="top" title="" aria-hidden="true" data-bs-original-title="Edit Image" aria-label="Edit Image"></i><span class="sr-only">Edit Image</span>
                     </label>
 
                     <input type="file" wire:model="photo" id="file-input" accept="image/*" class="d-none">
                     <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                      @livewire('photo.professor', ['professorId' => $professor->id])
+                      @if ($photo)
+                        <img src="{{ $photo->temporaryUrl() }}" alt="profile picture" class="w-100 border-radius-lg shadow-sm">
+                      @else
+                        @livewire('photo.professor', ['professorId' => $professor->id])
+
+                      @endif
                     </span>
                   </div>
                 </div>
@@ -45,7 +43,7 @@
               </div>
             </div>
             <div class="col-auto ms-auto">
-              <button class="btn bg-gradient-danger btn-sm mb-0" type="button"  onclick="confirm('Supprimer ce professeur ?') || event.stopImmediatePropagation()" wire:click="delete({{$professor->id}})">
+              <button class="btn bg-gradient-danger btn-sm mb-0" type="button" onclick="confirm('Supprimer ce professeur ?') || event.stopImmediatePropagation()" wire:click="delete({{ $professor->id }})">
                 Supprimer</button>
             </div>
           </div>
@@ -61,16 +59,14 @@
               <div class="col-md-6 align-self-center">
                 <label class="form-label" for="#firstName">First Name</label>
                 <div class="input-group ">
-                  <input wire:model="professor.firstname" id="firstName" name="firstName" class="form-control"
-                    type="text" placeholder="Robert" required="required">
+                  <input wire:model="professor.firstname" id="firstName" name="firstName" class="form-control" type="text" placeholder="Robert" required="required">
                   @error('professor.firstname') <div class="text-danger">{{ $message }}</div> @enderror
                 </div>
               </div>
               <div class="col-6">
                 <label class="form-label" for="#lastName">Last Name</label>
                 <div class="input-group ">
-                  <input wire:model="professor.lastname" id="lastName" name="lastName" class="form-control" type="text"
-                    placeholder="Martin" required="required">
+                  <input wire:model="professor.lastname" id="lastName" name="lastName" class="form-control" type="text" placeholder="Martin" required="required">
                   @error('professor.lastname') <div class="text-danger">{{ $message }}</div> @enderror
                 </div>
               </div>
